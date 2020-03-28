ef1d1f417806c624eac95f5f0fb74e98c98782f8
Martin Leydier <martin.leydier@gmail.com>
2020-03-28 04:07:59 +0100

    index: treat symlinks as files, do not follow them

--- src/app/filesystem.cr
+++ src/app/filesystem.cr
@@ -95,7 +95,7 @@ module FileSystem
   end
 
   def index(path, depth = 1, file_info = nil)
-    file_info = File.info(path) if file_info.nil?
+    file_info = File.info(path, false) if file_info.nil?
     if file_info.directory?
       this = FileSystemDirectory.new(path, file_info)
       if depth != 0
@@ -111,19 +111,19 @@ module FileSystem
   end
 
   private def sorted_indexable(entries, base_path)
-    visible = [] of Tuple(String, File::Info)
+    visible = Array(Tuple(String, File::Info)).new entries.size
     entries.each do |e|
       next if e[0] == '.'
       full_path = File.join(base_path, e)
       next unless File.readable? full_path
-      info = File.info full_path
+      info = File.info(full_path, false)
       visible << {full_path, info}
     end
     return visible.sort { |a, b| dir_sort(a[0], b[0], a[1], b[1]) }
   end
 
   # A comparison function to sort a directory content
-  # It does a case insensitive, directory first comparison
+  # It does a case sensitive, directory first comparison
   #
   # For example:
   #
@@ -136,9 +136,6 @@ module FileSystem
   # if a file_info_x argument is nil, then its corresponding path_x must be an absolute path or relative path,
   # it cannot be a simple filename
   private def dir_sort(path_1, path_2, file_info_1 = nil, file_info_2 = nil)
-    path_1 = path_1.downcase
-    path_2 = path_2.downcase
-
     is_dir_1 = if file_info_1.nil?
                  File.directory? path_1
                else
